067aae29a6b1dd2f62dd80976f77601de9b98999
justblack1556 <dorukunkd@gmail.com>
2025-06-25 20:19:14 +0300

    main-UI

--- scripts/entities/player/player.gd
+++ scripts/entities/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 class_name Player
 
+const SaveAndLoad = preload("res://scripts/singletons/save_and_load.gd")
 
 @export_category("Activity Controls")
 ## Whether the character is currently able to look around
@@ -41,31 +42,19 @@ class_name Player
 @export var friction : float = 6
 ## Bunnyhop window frame length
 @export var bhop_frames : int = 2
-## Whether bunnyhopping should be 'additive' - whether it should 
-## converge to the player's wishdir
 @export var additive_bhop : bool = true
-
 @export_category("Controlled Nodes")
-## Camera to update with mouse controls
 @export var camera : Camera3D
-## The PDA
 @export var pda: PDA
-
 @export_category("Debug")
-## Whether to look for and update debug raycasts
 @export var debug_mode_enabled : bool = false
-## Raycast to update with wishdir
 @export var debug_wishdir_raycast : RayCast3D
-## Raycast to update with velocity
 @export var debug_velocity_raycast : RayCast3D
 
-var can_move : bool = true : 
-	get:
-		return move_enabled and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
-	set(val):
-		can_move = val
 
-## Utility function for setting mouse mode, always visible if camera is unset
+var can_move : bool = true
+var frame_timer = 2
+
 func update_mouse_mode():
 	if look_enabled and camera:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -73,116 +62,122 @@ func update_mouse_mode():
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func mouse_look(event):
-	# Mouse look controls, don't activate if camera is unset
-	if look_enabled and can_move and camera:
-		if event is InputEventMouseMotion:
-			rotate_y(deg_to_rad(-event.relative.x * sensitivity.y))
-			camera.rotate_x(deg_to_rad(-event.relative.y * sensitivity.x))
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	if look_enabled and can_move and camera and event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sensitivity.y))
+		camera.rotate_x(deg_to_rad(-event.relative.y * sensitivity.x))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
-## Get player's intended direction. (0,0) if movement disabled
 func get_wishdir():
 	if not can_move:
 		return Vector3.ZERO
-	return Vector3.ZERO + \
-			(transform.basis.z * Input.get_axis(move_forward, move_backward)) +\
-			(transform.basis.x * Input.get_axis(move_left, move_right))
+	return (transform.basis.z * Input.get_axis(move_forward, move_backward)) + (transform.basis.x * Input.get_axis(move_left, move_right))
 
-## Get jump force
 func get_jump():
 	return sqrt(4 * jump_force * gravity)
 
-## Get gravity force
-func get_player_gravity(delta):
-	return gravity * delta
-
-######
-# All this code was only possible thanks to the technical writeup by Flafla2 available below.
-# Most of this was shamelessly adjusted from direct copy-pasting.
-#
-# Bunnyhopping from the Programmer's Perspective
-# https://adrianb.io/2015/02/14/bunnyhop.html
-######
-
-## Source-like acceleration function
 func accelerate(accelDir, prevVelocity, acceleration, max_vel, delta):
-	# Calculate projected velocity for the next frame
 	var projectedVel = prevVelocity.dot(accelDir)
-	# Calculate the accelerated velocity given the maximum velocity, projected velocity, and current acceleration
 	var accelVel = clamp(max_vel - projectedVel, 0, acceleration * delta)
-	# Return the previous velocity in addition to the new velocity post acceleration
 	return prevVelocity + accelDir * accelVel
 
-## Get intended velocity for the next frame
 func get_next_velocity(previousVelocity, delta):
 	var grounded = is_on_floor()
-	var can_jump = grounded # Jumping is a seperate var in case of additive bunnyhopping modifying grounded
-	
-	# Apply friction if player is grounded, and if the frame_timer indicates it should be applied
 	if grounded and (frame_timer >= bhop_frames):
 		var speed = previousVelocity.length()
 		if speed != 0:
 			var drop = speed * friction * delta
 			previousVelocity *= max(speed - drop, 0) / speed
-	else:
-		# If bunnyhopping is additive, we should use the air velocity and accelerate values for all frames
-		# that the bunnyhop is possible
-		if not additive_bhop:
-			grounded = false
-	
 	var max_vel = max_ground_velocity if grounded else max_air_velocity
 	var accel = ground_accelerate if grounded else air_accelerate
-	
-	# Calculate velocity for next frame
-	var velocity = accelerate(get_wishdir(), previousVelocity, accel, max_vel, delta)
-	# Apply gravity
-	velocity += Vector3.DOWN * get_player_gravity(delta)
-	
-	# Apply jump if desired
-	if (Input.is_action_pressed(jump) if jump_when_held else Input.is_action_just_pressed(jump)) \
-			and can_move and can_jump:
-		velocity.y = get_jump()
-	
-	# Return the new velocity
-	return velocity
+	var next_velocity = accelerate(get_wishdir(), previousVelocity, accel, max_vel, delta)
+	next_velocity += Vector3.DOWN * gravity * delta
+	if Input.is_action_just_pressed(jump) and can_move and grounded:
+		next_velocity.y = get_jump()
+	return next_velocity
 
-## Count of frames since last grounded
-var frame_timer = bhop_frames
-## Update frame timer if necessary
 func update_frame_timer():
 	if is_on_floor():
 		frame_timer += 1
 	else:
 		frame_timer = 0
 
-## Get frame velocity and update character body
 func handle_movement(delta):
-	# Update the bhop frame timer
-	update_frame_timer() 
+	if $logic/pause_menu.visible or $logic/settings.visible or $logic/save.visible:
+		return
+	update_frame_timer()
 	velocity = get_next_velocity(velocity, delta)
 	move_and_slide()
 
-## Conditionally update debug raycasts
-func draw_debug():
-	if not debug_mode_enabled:
-		return
-	var debug_velocity = velocity
-	## We don't usually want to visualize the y component here
-	debug_velocity.y = 0
-	# Print velocity in debug mode
-	print("BHop3D | Velocity: ", debug_velocity.length())
-	if debug_velocity_raycast: 
-		debug_velocity_raycast.target_position = debug_velocity
-	if debug_wishdir_raycast: 
-		debug_wishdir_raycast.target_position = get_wishdir()
-
-### Godot internal functions
 func _physics_process(delta):
+	if $logic/save.visible:
+		if Input.is_action_just_pressed("ui_cancel"):
+			$logic/save.visible = false
+			update_mouse_mode()
+		return
+	if Input.is_action_just_pressed("ui_cancel") and not $logic/settings.visible and not $logic/save.visible:
+		if $logic/pause_menu.visible:
+			$logic/pause_menu.visible = false
+			update_mouse_mode()
+		else:
+			$logic/pause_menu.visible = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_just_pressed("ui_cancel") and $logic/settings.visible:
+		$logic/settings.visible = false
+		update_mouse_mode()
+
 	handle_movement(delta)
-	draw_debug()
 
 func _unhandled_input(event):
+	if $logic/pause_menu.visible or $logic/settings.visible or $logic/save.visible:
+		return
 	mouse_look(event)
 
 func _ready():
 	update_mouse_mode()
+	var dir = DirAccess.open("res://save")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		var save_files = []
+		while file_name != "":
+			if not dir.current_is_dir():
+				save_files.append(file_name)
+			file_name = dir.get_next()
+		dir.list_dir_end()
+		$logic/save/load.clear()
+		for f in save_files:
+			$logic/save/load.add_item(f)
+
+func _on_load_button_pressed() -> void:
+	if $logic/save/load.text.strip_edges() != "":
+		var save_and_load = SaveAndLoad.new()
+		save_and_load.load($logic/save/load.text.strip_edges(), self)
+
+func _on_save_button_pressed() -> void:
+	if $logic/save/save.text.strip_edges() != "":
+		var save_and_load = SaveAndLoad.new()
+		save_and_load.save(self, get_tree().current_scene.scene_file_path, $logic/save/save.text.strip_edges())
+
+func _on_load_and_save_pressed() -> void:
+	$logic/save.visible = true
+
+func _on_continue_pressed() -> void:
+	$logic/pause_menu.visible=false
+	update_mouse_mode()
+
+func _on_new_game_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_settings_pressed() -> void:
+	$logic/settings.visible=true
+	$logic/pause_menu.visible=false
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	$logic/settings.visible=false
+	$logic/pause_menu.visible=true
+
+func _on_fov_slider_value_changed(value: float) -> void:
+	$Camera.fov = value
